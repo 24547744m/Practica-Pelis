82830d2fbaac3610a8f6457541bc03f1e3b9993b
Diego Zavaleta <24547744m@iespoblenou.org>
2015-11-03 23:31:33 +0100

    menú fragment creado y asignación a Java

--- app/src/main/java/com/diego/practica_pelis/PelisFragment.java
+++ app/src/main/java/com/diego/practica_pelis/PelisFragment.java
@@ -6,6 +6,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -32,7 +35,7 @@ public class PelisFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setHasOptionsMenu(true); //Seteamos como opciones de menú para llamarlo desde onCreateOptionsMenu
     }
 
     @TargetApi(Build.VERSION_CODES.M)// Debido a que esta funcionalidad esta disponible a partir de la API 23, nos recomienda poner esta anotación
@@ -47,11 +50,44 @@ public class PelisFragment extends Fragment {
         //datos del fragment y luego se extrae el contenido del atributo que deseemos, en este caso una ListView, buscandolo por su id.
         ListView lvPelis = (ListView) rootView.findViewById(R.id.lvPelis);
 
-        arrList = new ArrayList<>(Arrays.asList(datos));
-        adapter = new ArrayAdapter<String>(getContext(), R.layout.pelis_row, R.id.tvPeli, arrList);// para getContext(); es necesaria la anotacion en la parte superior del método
+        arrList = new ArrayList<>(Arrays.asList(datos));// Se setea al parámetro del constructor del ArrayList el array de String de películas de ejemplo creado anteriormente.
+                                                        // Transformandolo en un objeto del tipo List, usando la utilidad de la clase Arrays.
+        adapter = new ArrayAdapter<String>(getContext(), R.layout.pelis_row, R.id.tvPeli, arrList);// para getContext(); es necesaria la anotación en la parte superior del método
         lvPelis.setAdapter(adapter);
 
         return rootView; //retornamos el objeto antes creado y modificado
+
+    }
+
+/**
+ * Al crearse las opciones del menú, referenciamos el menú creado en el XML 'menu_fragment_pelis'
+ * */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_fragment_pelis, menu);//Referencia al menú del XML
+    }
+/**
+ * Aquí capturamos los eventos del menú, al ser seleccionados
+ * */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int menuId = item.getItemId(); //Obtenemos el id del menú clickado
+
+        if (menuId == R.id.menuRefresh){//Con el id obtenido comprobamos si es el el menú 'menuRefresh'
+            refresh(); //llamamos a nuestro método creado para refrescar nuestra activity
+            
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void refresh(){
+
 
     }
 
